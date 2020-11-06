6f35f6bc803e42f147db90e36cc2a0ab2fb33a8f
Lucilaxavier <lucilaxaviers@gmail.com>
2020-11-06 16:58:45 -0300

    primeiro commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,19 @@
-const cipher = {
-  // ...
-};
+//A B C D E F G H I J K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z
+//0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+
+
+function codificar() {
+  const frase1 = document.getElementById('palavra a cifrar digitada').value;
+  const offset = Numero(document.getElementById('deslocamento')).value;
+    console.log(typeof offset);
+  let codificarfrase1 = '';
+
+  for (let i=0; i<frase1.length; i++) {
+    let asciiNum = frase1[i].charCodeAt();
+    if (asciiNum >== 65 && asciiNum <= 90) {
+       salved+= String.fromCharCode(asciiNum + 13);
+    }
+  } 
+}
 
 export default cipher;
