8c8a9f77feaf0c7558ac65a3b8aa5560720c919e
Lucilaxavier <lucilaxaviers@gmail.com>
2020-11-06 16:58:45 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+import cipher from '.cipher.js';
+
+console.log(cipher);
+
+let criptografar = frase1(pompt("Digite aqui a frase que deseja codoficar"));
+let descriptografar = frase2(prompt("Digite aqui a frase que deseja descodificar"));
+
+let escolher = parseInt(
+   prompt (1- Deseja Criptografar \2- deseja descriptografar)
+   )
+   if (opcao === 1) {
+       let Palavra1 = prompt('Digite a palavra para Criptografar !!');
+       cipher(alfabeto, Palavra1);
+   } else if (opcao === 2) {
+       let decifrar = prompt('Digite a palavra para descriptografar !!');
+       decipher(alfabeto, decifrar);
+   }
+   
+   f"); 
+if (opcao == "Frase1") {
+
+    for (let i-0; i<str.length; i++) {
+        let asciiNum = str[i].charCodeAt();
+        if (asciiNum >= 65 && asciiNum <= 90) {
+           Palavra1+= cifrar.fromCharCode(asciiNum + 13);
+        }
+    document.getElementById("palavra a cifrar digitada").innerHTML=pergunta1;
+}
+
+}
+
 import cipher from './cipher.js';
 
 console.log(cipher);
