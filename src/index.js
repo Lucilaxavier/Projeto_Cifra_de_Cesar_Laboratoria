ec927d027ab2093ddaf8c9ef2116bf347cc993f3
Lucilaxavier <lucilaxaviers@gmail.com>
2020-11-07 17:07:30 -0300

    arrumei a lógica do cipher, add botões no HTML

--- src/index.js
+++ src/index.js
@@ -1,34 +1,20 @@
-import cipher from '.cipher.js';
-
+const cipher = {
+  encode: (codificarfrase, offset)=> {
+let codificarfrase = document.getElementById('palavra a cifrar digitada').value;
+let offset = Numero(document.getElementById('deslocamento')).value;
 console.log(cipher);
 
-let criptografar = frase1(pompt("Digite aqui a frase que deseja codoficar"));
-let descriptografar = frase2(prompt("Digite aqui a frase que deseja descodificar"));
+let encode = ("");
+let numero = ("");
+let opcao = ""
 
-let escolher = parseInt(
-   prompt (1- Deseja Criptografar \2- deseja descriptografar)
-   )
-   if (opcao === 1) {
-       let Palavra1 = prompt('Digite a palavra para Criptografar !!');
-       cipher(alfabeto, Palavra1);
-   } else if (opcao === 2) {
-       let decifrar = prompt('Digite a palavra para descriptografar !!');
-       decipher(alfabeto, decifrar);
-   }
-   
-   f"); 
-if (opcao == "Frase1") {
-
-    for (let i-0; i<str.length; i++) {
-        let asciiNum = str[i].charCodeAt();
-        if (asciiNum >= 65 && asciiNum <= 90) {
-           Palavra1+= cifrar.fromCharCode(asciiNum + 13);
+if (opcao === "encode") {
         }
-    document.getElementById("palavra a cifrar digitada").innerHTML=pergunta1;
+    document.getElementById("palavra a cifrar digitada").innerHTML;
 }
 
 }
 
-import cipher from './cipher.js';
+//import cipher from './cipher.js';
 
 console.log(cipher);
